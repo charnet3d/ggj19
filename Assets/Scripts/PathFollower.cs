753a07dafe6eed276f7a707d697aa216046dad33
charnet3d <charnet3d@gmail.com>
2019-01-26 19:16:25 +0100

    Added first snake predator with path follow script
     to go up the tree

---
+++ Assets/Scripts/PathFollower.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathFollower : MonoBehaviour
+{
+    [SerializeField]
+    float moveSpeed = 0;
+
+    [SerializeField]
+    GameObject path;
+
+    Transform tr;
+    PathNode[] nodes;
+    int currentNode = 0;
+    float timer;
+    static Vector3 currentNodePosition;
+    Vector3 startPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tr = GetComponent<Transform>();
+        nodes = path.GetComponentsInChildren<PathNode>();
+        CheckNode();
+    }
+
+    void CheckNode()
+    {
+        timer = 0;
+        currentNodePosition = nodes[currentNode].GetPosition();
+        startPosition = tr.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime * moveSpeed;
+
+        if (tr.position != currentNodePosition)
+        {
+            tr.position = Vector3.Lerp(startPosition, currentNodePosition, timer);
+        }
+        else
+        {
+            if (currentNode < nodes.Length - 1)
+            {
+                currentNode++;
+                CheckNode();
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/PathNode.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathNode : MonoBehaviour
+{
+    Transform tr;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tr = GetComponent<Transform>();
+    }
+
+    public Vector3 GetPosition()
+    {
+        return tr.position;
+    }
+}
