6d0e216c674c5830349b02a6c723f0a82c7a6b89
charnet3d <charnet3d@gmail.com>
2019-01-27 15:34:01 +0100

    Snake Path Follow
    Started some code to make the bird attack and snake fall

--- Assets/Scripts/MovementScript.cs
+++ Assets/Scripts/MovementScript.cs
@@ -8,13 +8,48 @@ public class MovementScript : MonoBehaviour
     Transform tr;
 
     [SerializeField]
-    float speed = 0;
+    float speed = 10;
+
+    [SerializeField]
+    float dashSpeed = 10.0f;
+
+    [SerializeField]
+    float maxDashTime = 10.0f;
+
+    [SerializeField]
+    float dashStoppingSpeed = 0.1f;
+
+    Vector3 moveDirection;
+    float currentDashTime;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         tr = GetComponent<Transform>();
+
+        currentDashTime = maxDashTime;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("Fire");
+            currentDashTime = 0.0f;
+        }
+
+        if (currentDashTime < maxDashTime)
+        {
+            moveDirection = new Vector3(0, 0, dashSpeed);
+            currentDashTime += dashStoppingSpeed;
+        }
+        else
+        {
+            moveDirection = Vector3.zero;
+        }
+
+        rb.velocity += new Vector2(moveDirection.x, moveDirection.y) * Time.deltaTime;
     }
 
     // Update is called once per frame
@@ -22,8 +57,6 @@ public class MovementScript : MonoBehaviour
     {
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
-
-        Debug.Log(moveHorizontal);
 
         Vector3 movement = new Vector2(moveHorizontal, moveVertical);
 
--- Assets/Scripts/PathFollower.cs
+++ Assets/Scripts/PathFollower.cs
@@ -8,20 +8,29 @@ public class PathFollower : MonoBehaviour
     float moveSpeed = 0;
 
     [SerializeField]
-    GameObject path;
+    GameObject path = null;
 
     Transform tr;
     PathNode[] nodes;
+    Animator anim;
+
     int currentNode = 0;
     float timer;
     static Vector3 currentNodePosition;
+    Quaternion currentNodeRotation;
     Vector3 startPosition;
+    Vector3 previousPosition;
+    Quaternion startRotation;
+
 
     // Start is called before the first frame update
     void Start()
     {
         tr = GetComponent<Transform>();
         nodes = path.GetComponentsInChildren<PathNode>();
+        anim = GetComponent<Animator>();
+
+        previousPosition = Vector3.zero;
         CheckNode();
     }
 
@@ -30,6 +39,9 @@ public class PathFollower : MonoBehaviour
         timer = 0;
         currentNodePosition = nodes[currentNode].GetPosition();
         startPosition = tr.position;
+        startRotation = tr.rotation;
+        currentNodeRotation = Quaternion.Euler(0, 0, Vector3.Angle(currentNodePosition - startPosition, 
+            Vector3.right));
     }
 
     // Update is called once per frame
@@ -40,14 +52,24 @@ public class PathFollower : MonoBehaviour
         if (tr.position != currentNodePosition)
         {
             tr.position = Vector3.Lerp(startPosition, currentNodePosition, timer);
+            tr.rotation = Quaternion.Lerp(startRotation, currentNodeRotation, timer);
         }
         else
         {
             if (currentNode < nodes.Length - 1)
             {
                 currentNode++;
+
+                if (currentNode > 0)
+                    previousPosition = startPosition;
+
                 CheckNode();
             }
+        }
+
+        if (currentNode >= nodes.Length - 1)
+        {
+            anim.Play("Idle");
         }
     }
 }
